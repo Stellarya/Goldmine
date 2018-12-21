e4430cb29d7bd959a57e4f0150e89d17e821a5e2
Stellarya <evanno.marie@gmail.com>
2018-12-21 13:05:13 +0100

    commentaire java fait (ouvre et ferme)

--- js/goldmine.js
+++ js/goldmine.js
@@ -333,6 +333,39 @@ $(document).ready(function () {
         $(this).parents('.rating').find('.rating-star').removeClass('checked');
     });
 
+    <!-- ************* modal box ******************* -->
+
+    // Get the modal
+    var modal = document.getElementById('myModalJava');
+
+// Get the button that opens the modal
+    var btn = document.getElementById("commentaire-btn");
+
+// Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    var submt = document.getElementsByClassName("btn_valider_comment")[0];
+
+// When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+// When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    submt.onclick = function() {
+        modal.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
     <!-- ************* input fonction ******************* -->
 
     $('input#java-1').on('input', function() {
