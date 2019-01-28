dcd3871a1abe13be5f800e2c578f845d728ce17e
Stellarya <evanno.marie@gmail.com>
2019-01-28 17:18:30 +0100

    test json presque concluant

--- js/jquery_research.js
+++ js/jquery_research.js
@@ -2,9 +2,22 @@ $(document).ready(function () {
     var jsonContent;
 
 
-    $.getJSON("rechercheGoldmine.json", function(parsedJson){
+    function afficherDev(jsonContent, nbr, nbrComp) {
+        $("#nomTrouve").text(jsonContent['devs'][nbr]['nom']);
+        $("#prenomTrouve").text(jsonContent['devs'][nbr]['prenom']);
+        $("#fctTrouve").text(jsonContent['devs'][nbr]['fonction']);
+        $("#comp1trouve").text(jsonContent['devs'][nbr]['Comps'][nbrComp]);
+        $("#comp2trouve").text(jsonContent['devs'][nbr]['Comps'][nbrComp]);
+        $("#comp3trouve").text(jsonContent['devs'][nbr]['Comps'][nbrComp] );
+    }
+
+    $.getJSON("goldmine.json", function(parsedJson){
         console.log(parsedJson);
         jsonContent = parsedJson;
-        
+        afficherDev(jsonContent, 0, 0);
+        /*$(".questionTotal").text(jsonContent['questions'].length)*/
+
     });
+
+
 });
