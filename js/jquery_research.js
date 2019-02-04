3f98a548f42ddff3581d16c07f193c9f4d34ae66
Stellarya <evanno.marie@gmail.com>
2019-02-04 09:42:17 +0100

    semaine2

--- js/jquery_research.js
+++ js/jquery_research.js
@@ -1,32 +1,76 @@
 $(document).ready(function () {
     var jsonContent;
 
-    /*function afficherDev(jsonContent, nbr, nbrComp) {
-        $("#nomTrouve").text(jsonContent['devs'][nbr]['nom']);
-        $("#prenomTrouve").text(jsonContent['devs'][nbr]['prenom']);
-        $("#fctTrouve").text(jsonContent['devs'][nbr]['fonction']);
-        $("#comp1trouve").text(jsonContent['devs'][nbr]['Comps'][nbrComp]);
-        $("#comp2trouve").text(jsonContent['devs'][nbr]['Comps'][nbrComp]);
-        $("#comp3trouve").text(jsonContent['devs'][nbr]['Comps'][nbrComp] );
-    }*/
-
-    $.getJSON("goldmine.json", function( json ) {
-        for (var i = 0; i < json["devs"].length; i++) {
-
-            for (var j = 0; j < json["devs"][i]['Comps'].length; j++) {
-                tr = $('<tr/>');
-                tr.append("<td>" + json["devs"][i]["nom"] + "</td>");
-                tr.append("<td>" + json["devs"][i]["prenom"] + "</td>");
-                tr.append("<td>" + json["devs"][i]["fonction"] + "</td>");
-                tr.append("<td>" + json["devs"][i]['Comps'][j]["Comp 1"] + "</td>");
-                tr.append("<td>" + json["devs"][i]['Comps'][j]["Comp 2"] + "</td>");
-                tr.append("<td>" + json["devs"][i]['Comps'][j]["Comp 3"] + "</td>");
-                tr.append("<td>" + json["devs"][i]['Comps'][j]["Comp 4"] + "</td>");
-
-                $('table').append(tr);
-            }
-        }
+    $.getJSON("goldmine.json", function (json) {
+        jsonContent = json;
+        console.log(json);
+        genererTabDev(jsonContent["devs"]);
     });
 
+    function genererTabDev (devs) {
+
+        var table_comp = $("#table_comps");
+
+        //On vide le tableau
+        table_comp.find("tr:gt(0)").remove();
+
+        for (var i = 0; i < devs.length; i++) {
+
+            tr = $('<tr/>');
+            tr.append("<td>" + devs[i]["Nom"] + "</td>");
+            tr.append("<td>" + devs[i]["Prenom"] + "</td>");
+            tr.append("<td>" + devs[i]["Fonction"] + "</td>");
+
+            for (var j = 0; j < devs[i]['Comps'].length; j++) {
+                tr.append("<td>" + devs[i]['Comps'][j] + "</td>");
+            }
+            table_comp.append(tr)
+        }
+    };
+
+    $("#button-search").click(function () {
+        var parsedAndFilteredJson = [];
+
+        var indexHeader=0;
+        //vider le header pour les compétences
+        $(".competence_tag_search").text("");
+        //Remplir le header pour les compétences
+        $(".qtagselect__choice span").each(function (index_json) {
+            indexHeader++;
+            //on met à jour le header avec le nom des tags séléctionnés
+            $("#competence_tag_search_col"+indexHeader).text($(this)[0].innerText);
+        });
+
+        //boucle sur les devs du json
+        $.each(jsonContent["devs"], function (index, obj) {
+
+            //boucle sur les comps d'un dev du json
+            $.each(obj.Comps, function (index_comp, obj_comp) {
+
+                var arreter = false;
+
+                //boucle sur les tags selectionnés
+                $(".qtagselect__choice span").each(function (index_json) {
+                    //TODO console.log pour voir obj_comp qd json aura changé
+                    //obj_comp.nomComp.toUpperCase
+                    //comparaison entre le json et le tag
+
+                    //dès qu'un dev a au moins une comp correspondant à un tag select, il apparait et pas la peine de continuer la boucle
+                    if (obj_comp['nomComp'].toUpperCase() === $(this)[0].innerText.toUpperCase()) {
+                        parsedAndFilteredJson.push(obj);
+                        arreter = true;
+                        return false;
+                    }
+                });
+
+                //si le dev est là, on arrête de regarder ses comps et on passe au dev suivant
+                if (arreter) {
+                    return false;
+                }
+
+            });
+        });
+        genererTabDev(parsedAndFilteredJson);
+    });
 
 });
