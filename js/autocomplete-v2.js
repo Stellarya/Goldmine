b93e108dbea21093fe55ef7773ab563eb3297ebc
Stellarya <evanno.marie@gmail.com>
2019-04-15 11:54:03 +0200

    atl TBD Appli faite

--- js/autocomplete-v2.js
+++ js/autocomplete-v2.js
@@ -1,15 +1,29 @@
 $(document).ready(function() {
-    var tTech = ["Angular","Delphi", "Spring", "HTML", "ESI", "Agranet", "J2EE"];
+    var tTech = ["Angular","Delphi", "Spring", "HTML", "J2EE"];
     $('#autoSuggest').autocomplete({
         source: tTech,
         select: showLabel,
         change: showLabel
-    })
+    });
 
     function showLabel(event, ui) {
         $('.colAngular').hide();
         $('.angularJS').show();
+    };
+    var aApp = ["SPIE-Inventaire","BCELO-BSM", "BCELO-ActivParage", "SPIE-PointFocal", "Etoile"];
+    $('#autoSuggestApp').autocomplete({
+        source: aApp,
+        select: showLabelApp,
+        change: showLabelApp
+    });
+
+    function showLabelApp(event, ui) {
+        $('.col-BSM').show();
+        $('.rawBSM').show();
+        $('.raw4bcelo').hide();
+        $('.col-SPIE').hide();
+        $('.rawSPIE').hide();
+        $("#checkspie").prop('checked', false);
+        $("#checkbsm").prop('checked', true);
     }
-
-
 });
